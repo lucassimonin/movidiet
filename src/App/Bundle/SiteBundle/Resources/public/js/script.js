c884b93cdb5f4f754f32ed48fc37c31f77503503
Simonin Lucas <lucas.simonin@cgi.com>
2017-02-16 19:21:17 +0100

    fix issue menu

--- src/App/Bundle/SiteBundle/Resources/public/js/script.js
+++ src/App/Bundle/SiteBundle/Resources/public/js/script.js
@@ -6,7 +6,7 @@
   $('a.page-scroll').bind('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top - 50)
+      scrollTop: ($($anchor.attr('href')).offset().top)
     }, 1250, 'easeInOutExpo');
     event.preventDefault();
   });
