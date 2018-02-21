d6f71c74f801dfae52217ef9c4f0fb5ce51f6a73
Lucas SIMONIN <lsimonin2@gmail.com>
2018-02-21 22:36:53 +0100

    fix issue

--- src/App/Bundle/SiteBundle/Controller/ParentController.php
+++ src/App/Bundle/SiteBundle/Controller/ParentController.php
@@ -4,6 +4,7 @@ namespace App\Bundle\SiteBundle\Controller;
 
 use App\Bundle\SiteBundle\Helper\CoreHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class ParentController extends Controller
 {
